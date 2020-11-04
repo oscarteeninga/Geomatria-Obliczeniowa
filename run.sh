d608c635d73fb8f856d0fd2ca3aafb827abcb1fc
Oscar Teeninga <45202578+oscarteeninga@users.noreply.github.com>
2020-11-04 22:14:29 +0100

    y-monotone and points qualification

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-. venv/bin/activate
+. geometria/bin/activate
 jupyter notebook
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip3 install virtualenv --user
-python3 -m virtualenv venv
-. venv/bin/activate
+python3 -m virtualenv geometria
+. geomatria/bin/activate
 pip3 install -r requirements.txt
-python3 -m ipykernel install --user --name=venv
+python3 -m ipykernel install --user --name=geometria
